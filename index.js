6ecb676e29eb51ef96e639f5b9c02e49867e994f
vikram(wsl) <vikramjit360@gmail.com>
2024-07-15 23:20:06 +0530

    refactor index.js to handle POST requests and change code structure

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
 require('dotenv').config();
+const PORT = process.env.PORT;
+
 const express = require('express')
 const cors = require("cors");
 const app = express()
 
 const Note = require('./models/note');
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
+app.use(express.static("dist"));
+app.use(express.json())
+app.use(cors());
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -30,18 +18,7 @@ const requestLogger = (request, response, next) => {
   console.log('---')
   next()
 }
-
-app.use(express.static("dist"));
-
-app.use(express.json())
-
-app.use(cors());
-
 app.use(requestLogger)
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -54,42 +31,27 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    console.log('x')
-    response.status(404).end()
-  }
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -99,9 +61,11 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
