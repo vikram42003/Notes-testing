506ea3227cbd65f46424a74abc1fbe53a34c9f8d
vikram(wsl) <vikramjit360@gmail.com>
2024-08-03 19:26:37 +0530

    feat: add users and user authentication

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 require("express-async-errors");
 const app = express();
 const cors = require("cors");
+const loginRouter = require("./controllers/login");
+const usersRouter = require("./controllers/users");
 const notesRouter = require("./controllers/notes");
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
@@ -26,6 +28,8 @@ app.use(express.static("dist"));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use("/api/login", loginRouter);
+app.use("/api/users", usersRouter);
 app.use("/api/notes", notesRouter);
 
 app.use(middleware.unknownEndpoint);
